8ea1a02982a114444c1dbc41e1ee466fc5f8f963
Pham Hung <phamhung20022015@gmail.com>
2022-01-30 22:22:54 +0700

    fix install.bat to use powershell

--- install.bat
+++ install.bat
@@ -1,11 +1,7 @@
 @echo off
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe";Set-ExecutionPolicy RemoteSigned -scope CurrentUser;Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'); exit
-scoop install git 
-scoop update 
-scoop install vim nodejs python go rustup llvm -y
-vim -c ":PlugInstall | :qa!"
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"; Set-ExecutionPolicy RemoteSigned -scope CurrentUser; Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'); scoop install git; git config --global credential.helper manager-core; scoop update; scoop install vim nodejs python go rustup llvm; vim -c ":PlugInstall | :qa!"
 echo.------------------------------------------------------------
 echo.INSTALLATION DONE!!! RESTART TERMINAL AND ENJOY"<3"
 echo.------------------------------------------------------------
 pause
-exit /b 0
+exit 
