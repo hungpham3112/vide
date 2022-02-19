fb51cfd603ffd2df55b86efd53427ab2b9b353ee
Pham Hung <phamhung20022015@gmail.com>
2022-02-19 12:12:27 +0700

    Fix not found node when installing

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,15 +1,12 @@
 Set-PSDebug -Off
-function refreshenv {
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-}
 iwr -useb get.scoop.sh | iex
 git config --global --unset credential.helper
 scoop update
 scoop bucket add nerd-fonts
 scoop install cousine-NF-Mono 2>$null
 scoop install vim nodejs 
-refreshenv
 md $env:LOCALAPPDATA\coc -Force >$null 2>$null
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") >$null
 gvim -c ":PlugInstall | :qa!"
 $FinalMessage = @"
 ------------------------------------------------------------
