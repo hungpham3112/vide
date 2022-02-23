87fdcc3202c974553a2c5f1ea51f46047beddf0a
HungPham <phamhung20022015@gmail.com>
2022-02-22 18:47:14 -0800

    add cmd.exe /c to open gvim

--- bin/install.ps1
+++ bin/install.ps1
@@ -7,7 +7,7 @@ scoop install cousine-NF-Mono 2>$null
 scoop install vim nodejs 
 md $env:LOCALAPPDATA\coc -Force >$null 2>$null
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") >$null
-gvim -c ":PlugInstall | :qa!"
+cmd.exe /c gvim -c ":PlugInstall | :qa!"
 $FinalMessage = @"
 ------------------------------------------------------------
 -----INSTALLATION DONE!!! RESTART TERMINAL AND ENJOY<3-----
