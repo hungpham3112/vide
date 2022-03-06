3c681e1104bab79b2c8a28783e48122cf47be25b
hungpham <phamhung20022015@gmail.com>
2022-03-06 20:40:54 +0700

    install.ps2 ver 0.1.2

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,77 +1,117 @@
 Set-PSDebug -Off
 
 $VIMFILES_PATH = "$env:userprofile/vimfiles/"
-$COC_PATH = "$env:LOCALAPPDATA/coc" 
+$COC_PATH = "$env:LOCALAPPDATA/coc"
+$FinalMessage = @"
+------------------------------------------------------------
+-----INSTALLATION DONE!!! RESTART TERMINAL AND ENJOY <3-----
+------------------------------------------------------------
+"@
 
-Write-Host "Welcome to VIDE!"
-Write-Host "Checking requirements..."
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-if (Get-Command scoop -ErrorAction SilentlyContinue) {
-    Write-Host "Scoop is already installed ✅"
-} else {
-    try {
-        Write-Host "Installing scoop..."
-        iwr -useb get.scoop.sh | iex
+$greenCheck = @{
+  Object = [Char]8730
+  ForegroundColor = 'Green'
+  NoNewLine = $true
+  }
+
+function InstallScoop {
+    if (Get-Command scoop -ErrorAction SilentlyContinue) {
+        Write-Host "Scoop is already installed " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    } else {
+        try {
+            Write-Host "Installing scoop..."
+            iwr -useb get.scoop.sh | iex
+        }
+        catch {
+            Write-Host "An error occurred while installing scoop. Re-installing..."
+            rd $env:USERPROFILE/scoop -Recurse -Force >$null 2>$null
+        }
     }
-    catch {
-        Write-Host "An error occurred while installing scoop. Re-installing..."
-        rd $env:USERPROFILE/scoop -Recurse -Force >$null 2>$null
+}
+
+function InstallGit {
+    if (Get-Command git -ErrorAction SilentlyContinue) {
+        Write-Host "Git is already installed " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    } else {
+        scoop install git >$null
+        git config --system --unset credential.helper >$null 2>$null
+        Write-Host "Install git successfully " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
     }
 }
 
-if (Get-Command git -ErrorAction SilentlyContinue) {
-    Write-Host "Git is already installed ✅"
-} else {
-    scoop install git >$null
-    git config --global --unset credential.helper >$null 2>$null
-    Write-Host "Install git successfully ✅"
-}
-if (![System.IO.Directory]::Exists($VIMFILES_PATH)) {
-    git clone https://github.com/hungpham3112/vide.git $vimfiles_path
+function CloneRepo {
+    if (![System.IO.Directory]::Exists($VIMFILES_PATH)) {
+        git clone https://github.com/hungpham3112/vide.git $vimfiles_path
+    }
 }
 
-if (Get-Command node -ErrorAction SilentlyContinue) {
-    Write-Host "Nodejs is already installed ✅"
-} else {
-    scoop install nodejs >$null
-    Write-Host "Install nodejs successfully ✅"
+function InstallNodejs {
+    if (Get-Command node -ErrorAction SilentlyContinue) {
+        Write-Host "Nodejs is already installed " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    } else {
+        scoop install nodejs >$null
+        Write-Host "Install nodejs successfully " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    }
 }
 
-if (Get-Command python -ErrorAction SilentlyContinue) {
-    Write-Host "Python is already installed ✅"
-} else {
-    scoop install python >$null
-    Write-Host "Install python successfully ✅"
+function InstallPython {
+    if (Get-Command python -ErrorAction SilentlyContinue) {
+        Write-Host "Python is already installed " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    } else {
+        scoop install python >$null
+        Write-Host "Install python successfully " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    }
 }
 
-if (Get-Command vim -ErrorAction SilentlyContinue) {
-    Write-Host "Python is already installed ✅"
-} else {
-    scoop bucket add versions
-    scoop install vim-nightly >$null
-    Write-Host "Install vim-nightly successfully ✅"
+function InstallVim {
+    if (Get-Command vim -ErrorAction SilentlyContinue) {
+        Write-Host "Python is already installed " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    } else {
+        scoop bucket add versions
+        scoop install vim-nightly >$null
+        Write-Host "Install vim-nightly successfully " -ForegroundColor Green -NoNewline
+        Write-Host @greenCheck
+    }
 }
 
 function UpdateScoop {
     scoop update
 }
 
-function Install_Nerd_Fonts {
+function InstallNerdFonts {
     scoop bucket add nerd-fonts
     scoop install cousine-NF-Mono 2>$null
-    Write-Host "Install nerd-fonts successfully ✅"
+    Write-Host "Install nerd-fonts successfully " -ForegroundColor Green -NoNewline
+    Write-Host @greenCheck
 }
 
-if (![System.IO.Directory]::Exists($COC_PATH)) {
-    md $COC_PATH -Force >$null 2>$null
+function CreateCocPath {
+    if (![System.IO.Directory]::Exists($COC_PATH)) {
+        md $COC_PATH -Force >$null 2>$null
+    }
 }
 
-start ~\scoop\shims\gvim.exe
-$FinalMessage = @"
-------------------------------------------------------------
------INSTALLATION DONE!!! RESTART TERMINAL AND ENJOY <3-----
-------------------------------------------------------------
-"@
-Write-Output $FinalMessage
-
-
+function main {
+    Write-Host "Welcome to VIDE" -ForegroundColor Green -NoNewline
+    Write-Host "Checking requirements.." -ForegroundColor Green -NoNewline
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+    InstallScoop
+    UpdateScoop
+    InstallGit
+    CloneRepo
+    InstallNerdFonts
+    InstallNodejs
+    InstallPython
+    InstallVim
+    CreateCocPath
+    start ~\scoop\shims\gvim.exe
+    Write-Output $FinalMessage -ForegroundColor Green
+}
