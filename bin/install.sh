6be2dc48e483f08791f6b1262e99c9c235fc8e96
HungPham <phamhung20022015@gmail.com>
2023-12-05 10:41:05 +0700

    fix(bin): nodejs download deprecated script

--- bin/install.sh
+++ bin/install.sh
@@ -4,10 +4,11 @@ GREEN='\033[0;32m' # Green Color
 RED='\033[0;31m' # Red Color
 NC='\033[0m' # No Color
 CHECK_DONE='\xE2\x9C\x94'
+NODE_MAJOR=21
 
 function update_system () {
     echo -e "${GREEN}UPDATING SYSTEM...${NC}"
-    sudo apt upgrade -y && sudo apt update -y
+    sudo apt update -y && sudo apt upgrade -y
     echo -e "${GREEN}SUCCESSFULLY UPDATE SYSTEM ${CHECK_DONE}${NC}"
 }
 
@@ -41,7 +42,11 @@ function install_vim() {
 
 function install_nodejs() {
     echo -e "${GREEN}INSTALLING NODEJS...${NC}"
-    sudo curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
     sudo apt-get install nodejs -y
     echo -e "${GREEN}INSTALL NODEJS DONE ${CHECK_DONE}${NC}"
 }
